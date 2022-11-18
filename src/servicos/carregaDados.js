5cfe7158afc34c4676d56c7c1de898e5a53a673c
Filipi Rafael <filipirafael.123@gmail.com>
2022-11-17 21:25:04 -0300

    Add new updates

---
+++ src/servicos/carregaDados.js
@@ -0,0 +1,10 @@
+import produtos from '../mocks/produtores';
+import topo from '../mocks/topo';
+
+export const carregaProdutores = () => {
+  return produtos;
+};
+
+export const carregaTopo = () => {
+  return topo;
+};
