5be7fab83c4acf30a2362d07ddc2a3d5d15cfd16
Filipi Rafael <filipirafael.123@gmail.com>
2022-11-20 09:49:08 -0300

    Add new updats

--- src/telas/Home/componentes/Topo.js
+++ src/telas/Home/componentes/Topo.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import {Text, View, Image, StyleSheet} from 'react-native';
 
-import logo from '../../../../assets/logo.png';
+import {carregaTopo} from '../../../servicos/carregaDados';
+import logo from '../../../assets/logo.png';
 
-export default function Topo() {
-  return (
-    <View style={estilos.topo}>
-      <Image style={estilos.imagem} source={logo} />
-      <Text style={estilos.boasVindas}>Olá Filipi</Text>
-      <Text style={estilos.lengenda}>Encontre os melhores produtores</Text>
-    </View>
-  );
+class Topo extends React.Component {
+  state = {
+    topo: {
+      boasVindas: '',
+      legenda: '',
+    },
+  };
+
+  atualizaTopo() {
+    const retorno = carregaTopo();
+    this.setState({topo: retorno});
+  }
+
+  componentDidMount() {
+    this.atualizaTopo();
+  }
+
+  render() {
+    return (
+      <View style={estilos.topo}>
+        <Image style={estilos.imagem} source={logo} />
+        <Text style={estilos.boasVindas}>{this.state.topo.boasVindas}</Text>
+        <Text style={estilos.lengenda}>{this.state.topo.legenda}</Text>
+      </View>
+    );
+  }
 }
 
 const estilos = StyleSheet.create({
@@ -33,3 +52,5 @@ const estilos = StyleSheet.create({
     lineHeight: 26,
   },
 });
+
+export default Topo;
